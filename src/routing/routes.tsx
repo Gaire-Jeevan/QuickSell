0b6045a8baae2e16cecc020dec7b6bfd845cea60
Jeevan <gairejeevan7@gmail.com>
2024-09-09 08:07:07 +0530

    Added error Page.

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,19 +5,22 @@ import LogInPage from "../pages/LogInPage";
 import SignUpPage from "../pages/SignUpPage";
 import ContactUsPage from "../pages/ContactUsPage";
 import UserDashboardPage from "../pages/UserDashboardPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
+  {
+    errorElement: <ErrorPage />,
+  },
   { path: "/", element: <HomePage /> },
   { path: "/login", element: <LogInPage /> },
   { path: "/signup", element: <SignUpPage /> },
   { path: "/home", element: <HomePage /> },
-  
+
   { path: "/signup", element: <SignUpPage /> },
   { path: "/signup", element: <SignUpPage /> },
   { path: "/sell", element: <AddEditProduct /> },
   { path: "/contactUs", element: <ContactUsPage /> },
   { path: "/dashboard", element: <UserDashboardPage /> },
-
 ]);
 
 export default router;
