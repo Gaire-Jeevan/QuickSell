c6625e641c7f9249b647b6ab7702385561a38af9
Jeevan <gairejeevan7@gmail.com>
2024-09-09 08:09:30 +0530

    Logic added to add Product.

--- src/components/AddEditProduct.tsx
+++ src/components/AddEditProduct.tsx
@@ -1,12 +1,58 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const AddEditProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
+
+  // Initialize cookies
+  const cookies = new Cookies();
+  const token = cookies.get("jwt_authorization"); // Get the token
+
+  const addEditProduct = async (e: any) => {
+    e.preventDefault();
+
+    const URL = "http://localhost:8080/api/products";
+
+    axios
+      .post(
+        URL,
+        {
+          name,
+          price,
+          category,
+          description,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Set the Authorization header
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        navigate("/home");
+      })
+      .catch((err) =>
+        console.error("Error adding product:", err.response?.data || err)
+      );
+  };
   return (
     <div className="flex justify-center m-3 lg:m-4">
       <div className="rounded-md shadow-lg w-[310px] md:w-[755px] lg:w-[1005px]  p-4 ">
         <h2 className="font-bold text-xl lg:text-2xl ml-[-10px]  lg:ml-[-3px]">
           Add/Edit Product
         </h2>
-        <form action="" className="flex flex-col justify-center  ml-[-10px] ">
+        <form
+          onSubmit={addEditProduct}
+          className="flex flex-col justify-center  ml-[-10px] "
+        >
           <div className="form-group mt-4 lg:mt-10 mb-3 text-sm lg:text-lg">
             <label
               htmlFor="productTitle"
@@ -20,7 +66,10 @@ const AddEditProduct = () => {
               id="productTitle"
               aria-describedby="emailHelp"
               maxLength={100}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               placeholder="Enter product name"
+              required
             />
           </div>
 
@@ -36,7 +85,10 @@ const AddEditProduct = () => {
               id="productDescription"
               rows={5}
               maxLength={75}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Describe the product details"
+              required
             ></textarea>
           </div>
 
@@ -47,7 +99,12 @@ const AddEditProduct = () => {
             >
               Category
             </label>
-            <select className="form-control">
+            <select
+              className="form-control"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option>Select</option>
               <option>Electronics</option>
               <option>Furniture</option>
               <option>Clothing</option>
@@ -64,100 +121,18 @@ const AddEditProduct = () => {
               className="form-control rouded-md h-12 text-sm"
               id="price"
               placeholder="0"
-            />
-          </div>
-
-          <div className="form-group mb-3 text-sm lg:text-lg">
-            <label htmlFor="uploadImage" className="text-base font-medium mb-2">
-              Upload Images
-            </label>
-            <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 lg:flex-row lg:space-y-0">
-              <input
-                type="file"
-                accept="image/*"
-                className="form-control-file"
-                id="exampleFormControlFile1"
-              />
-              <input
-                type="file"
-                accept="image/*"
-                className="form-control-file"
-                id="exampleFormControlFile2"
-              />
-              <input
-                type="file"
-                accept="image/*"
-                className="form-control-file"
-                id="exampleFormControlFile3"
-              />
-            </div>
-          </div>
-
-          <div className="form-group mb-3 text-sm lg:text-lg">
-            <label htmlFor="location" className="text-base font-medium mb-2">
-              Location
-            </label>
-            <input
-              type="text"
-              className="form-control rouded-md h-12 text-sm"
-              id="location"
-              maxLength={50}
-              placeholder="Enter product location"
-            />
-          </div>
-
-          <div className="form-group mb-3 text-sm lg:text-lg">
-            <label htmlFor="location" className="text-base font-medium mb-2">
-              Condition
-            </label>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="exampleRadios"
-                id="radioNew"
-                value="option1"
-              />
-              <label className="form-check-label" htmlFor="radioNew">
-                New
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="exampleRadios"
-                id="radioUsed"
-                value="option2"
-              />
-              <label className="form-check-label" htmlFor="radioUsed">
-                Used
-              </label>
-            </div>
-          </div>
-
-          <div className="form-group mb-3 text-sm lg:text-lg">
-            <label
-              htmlFor="stockQuantity"
-              className="text-base font-medium mb-2"
-            >
-              Stock Quantity
-            </label>
-            <input
-              type="number"
-              className="form-control rouded-md h-12 text-sm"
-              id="stockQuantity"
-              placeholder="Enter available quantity"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
             />
           </div>
 
           <div className="flex justify-end">
-            <button
+            {/* <button
               type="submit"
               className="btn btn-outline-secondary rounded-md text-black font-medium text-base lg:text-lg w-30 h-12  mt-4 lg:mt-10"
             >
               Save
-            </button>
+            </button> */}
             <button
               type="submit"
               className="btn btn-success rounded-md text-white font-medium text-base lg:text-lg h-12 w-30 ml-2 mt-4 lg:mt-10"
